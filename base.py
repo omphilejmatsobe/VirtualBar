e63d1f31b6b3645c6d3ac91e93ea82c9c096f90d
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-05-28 19:22:34 +0200

    DB class added

--- base.py
+++ base.py
@@ -13,6 +13,9 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SECRET_KEY'] = 'super user'
+db = SQLAlchemy(app)
+
+class UserDB(db.Model):
 
 
 @app.route('/')
