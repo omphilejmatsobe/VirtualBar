66e538786e0ceec5d4899ff46a03a26f965171d3
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-05-28 15:24:18 +0200

    Application Cleaned. Restart of project

--- README.md
+++
@@ -1,1 +0,0 @@
-This is the readme for the VirtualBar application. Will be updated soon.
--- console.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-"""
-This module contains the console class
-that manages the application
-"""
-
