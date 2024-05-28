bfaa5350f1d0589ed8451ec7e8b158146b20c36b
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-05-28 19:36:45 +0200

    CodeBase updated

--- base.py
+++ base.py
@@ -16,34 +16,38 @@ app.config['SECRET_KEY'] = 'super user'
 db = SQLAlchemy(app)
 
 class UserDB(db.Model):
-
+    id = sb.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=False, nullable=False )
+    userName = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Coilumn(db.Text, unique=True, nullable=False)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login/login.html')
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     return render_template('login/signup.html')
 
 
-@app.route('/recover')
+@app.route('/recover', methods=['POST'])
 def passwordRecovery():
     return render_templater('login/recover.html')
 
 
-@app.route('/session')
+@app.route('/session', methods=['GET', 'POST'])
 def session():
     return render_template('session/session.html')
 
 
-@app.route('/profile')
+@app.route('/profile', methods=['GET', 'POST'])
 def profile():
     return render_template('profile/profile.html')
 
